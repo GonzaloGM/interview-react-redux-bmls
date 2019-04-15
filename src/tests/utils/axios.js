4ef0894ccb840322bfe3f37e08ee580ad4455c20
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2019-04-15 10:54:26 -0300

    Tests for Codesandbox

--- src/tests/utils/axios.js
+++ src/tests/utils/axios.js
@@ -1,8 +1,8 @@
-// import $http from "axios/index";
+import $http from "axios/index";
 
-import axios from 'axios'
-axios.defaults.adapter = require('axios/lib/adapters/http')
-const $http = axios.create({
-  baseURL: 'http://localhost:3000/'
-})
+// import axios from 'axios'
+// axios.defaults.adapter = require('axios/lib/adapters/http')
+// const $http = axios.create({
+//   baseURL: 'http://localhost:3000/'
+// })
 export default $http
